3102bbe14e0f9dcabb44da5466bc41905e6f4658
Kane Rogers-Wong <kane.m.rogers@gmail.com>
2026-05-27 12:41:23 +1000

    Use Slang crate for compilation, stop tracking spv

--- src/main.rs
+++ src/main.rs
@@ -975,57 +975,51 @@ struct App {
 }
 
 fn compile_shaders() {
-    const SHADERS: &[(&str, &str, &str)] = &[
-        (
-            "shaders/raygen.slang",
-            "raygeneration",
-            "shaders/raygen.rgen.spv",
-        ),
-        ("shaders/miss.slang", "miss", "shaders/miss.rmiss.spv"),
-        (
-            "shaders/closesthit.slang",
-            "closesthit",
-            "shaders/closesthit.rchit.spv",
-        ),
-        (
-            "shaders/fullscreen.slang",
-            "vertex",
-            "shaders/fullscreen.vert.spv",
-        ),
-        (
-            "shaders/tonemapping.slang",
-            "fragment",
-            "shaders/tonemapping.frag.spv",
-        ),
+    use shader_slang as slang;
+
+    const SHADERS: &[(&str, &str)] = &[
+        ("shaders/raygen.slang", "shaders/raygen.rgen.spv"),
+        ("shaders/miss.slang", "shaders/miss.rmiss.spv"),
+        ("shaders/closesthit.slang", "shaders/closesthit.rchit.spv"),
+        ("shaders/fullscreen.slang", "shaders/fullscreen.vert.spv"),
+        ("shaders/tonemapping.slang", "shaders/tonemapping.frag.spv"),
     ];
 
-    for (input_path, stage, output_path) in SHADERS {
+    let global_session = slang::GlobalSession::new().unwrap();
+    let search_path = std::ffi::CString::new("shaders/").unwrap();
+
+    let session_options = slang::CompilerOptions::default()
+        .language(slang::SourceLanguage::Slang)
+        .optimization(slang::OptimizationLevel::None)
+        .debug_information(slang::DebugInfoLevel::Maximal)
+        .glsl_force_scalar_layout(true)
+        .matrix_layout_column(true);
+
+    let target_desc = slang::TargetDesc::default()
+        .format(slang::CompileTarget::Spirv)
+        .profile(global_session.find_profile("glsl_460"));
+
+    let targets = [target_desc];
+    let search_paths = [search_path.as_ptr()];
+    let session_desc = slang::SessionDesc::default()
+        .targets(&targets)
+        .search_paths(&search_paths)
+        .options(&session_options);
+
+    let session = global_session.create_session(&session_desc).unwrap();
+
+    for (input_path, output_path) in SHADERS {
         log::debug!("[SHADERS] Compiling {input_path} to {output_path}");
 
-        let status = std::process::Command::new("slangc")
-            .arg(format!("./{}", input_path))
-            .arg("-entry")
-            .arg("main")
-            .arg("-stage")
-            .arg(stage)
-            .arg("-target")
-            .arg("spirv")
-            .arg("-profile")
-            .arg("glsl_460")
-            .arg("-fvk-use-entrypoint-name")
-            .arg("-matrix-layout-column-major")
-            .arg("-fvk-use-scalar-layout")
-            .arg("-g")
-            .arg("-o")
-            .arg(output_path)
-            .spawn()
-            .unwrap()
-            .wait()
+        let module = session.load_module(input_path).unwrap();
+        let entry_point = module.find_entry_point_by_name("main").unwrap();
+        let program = session
+            .create_composite_component_type(&[module.clone().into(), entry_point.into()])
             .unwrap();
+        let linked_program = program.link().unwrap();
+        let shader_bytecode = linked_program.entry_point_code(0, 0).unwrap();
 
-        if !status.success() {
-            panic!("Failed to compile shader {input_path}");
-        }
+        std::fs::write(output_path, shader_bytecode.as_slice()).unwrap();
     }
 }
 
